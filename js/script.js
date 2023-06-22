8a098d7085a07c6cf0096c10fc3f00275fcd8ec6
Alena <alena.karpov@gmail.com>
2023-06-22 11:36:22 +0100

    feat: add js functions
    start, rememberMyFilms, detectPersonalLevel, writeYourGenres, showMyDB

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+	}
+}
+
+start();
     
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,10 +20,62 @@ const personalMovieDB = {
 	private: false
 };
 
-for (let i = 0; i < 2; i++ ) {
-	let movie = prompt('Один из последних просмотренных фильмов', '');
-	let rating = prompt('На сколько оцените его', '');
-	personalMovieDB.movies[movie] = rating;
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++ ) {
+		const movie = prompt('Один из последних просмотренных фильмов', '');
+		const rating = prompt('На сколько оцените его', '');
+		if (!movie || movie && movie.length > 50) {
+			if (!movie) {
+				console.log('film name must be greater then 0 characters');
+			} else {
+				console.log('movie name should be less than 50 characters');
+			}
+			i--;
+		} else {
+			personalMovieDB.movies[movie] = rating;
+		}
+	}
 }
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотрено довольно мало фильмов');
+	} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+		console.log('Вы классический зритель');
+	} else if (personalMovieDB.count > 30) {
+		console.log('Вы киноман');
+	} else {
+		console.log('Произошла ошибка');
+	}
+}
+
+detectPersonalLevel();
+
+function writeYourGenres() {
+
+	for (let i = 0; i < 3; i++) {
+		personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+	}
+}
+
+writeYourGenres();
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.private);
+
+
+
+
+
+
+
+
+
